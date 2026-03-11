eaece34f69d7e9fdfe5a9169c8ed20a53ea412e5
Miyagi <ra.mu.umy243@gmail.com>
2026-03-12 05:25:28 +0900

    768x compatible

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("header.html")
+    fetch("./header.html")
     .then(response => response.text())
     .then(data =>{
         document.getElementById("header").innerHTML = data;
     });
+});
+
+const hambuger = document.getElementById("hamburger");
+const navMenu = document.getElementsById("nav-menu");
+
+hambuger.addEventListener("click" , () =>{
+  navMenu.classList.toggle("active");  
 });
