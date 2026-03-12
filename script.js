8e331e6a3007c908632a91f96522ca8e7f3b2c83
Miyagi <ra.mu.umy243@gmail.com>
2026-03-12 14:34:32 +0900

    scheduleCommit

--- script.js
+++ script.js
@@ -8,9 +8,26 @@ window.addEventListener("DOMContentLoaded", () => {
         const navMenu = document.getElementById("nav-menu");
 
         hamburger.addEventListener("click" , () =>{
-        navMenu.classList.toggle("active");  
+            navMenu.classList.toggle("active");  
+        });
+
+        const buttons = document.querySelectorAll(".tab-btn");
+        const contents = document.querySelectorAll(".tab-content");
+
+        buttons.forEach(button => {
+
+            button.addEventListener("click", () => {
+
+                buttons.forEach(btn => btn.classList.remove("active"));
+                contents.forEach(content => content.classList.remove("active"));
+
+                button.classList.add("active");
+
+                const tab = button.dataset.tab;
+                document.getElementById(tab).classList.add("active");
+            });
+        });
     });
-});
 
 
 });
